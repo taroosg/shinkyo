054eb3eeef2d90eea5efe05e064bfcca6d4cabe9
taroosg <macchinetta.xyz@gmail.com>
2020-03-27 14:02:27 +0900

    update functions

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,11 @@ import shinkyo_text from './data/shinkyo_text.json';
 const App = () => {
 
   // 初期値の設定
+  // 正しい般若心経
   const correctText = shinkyo_text;
+  // 漢字のみ摘出した般若心経
   const onlyText = correctText.filter(x => !(x.char === "・" || x.char === "、" || x.char === "。"));
+  // 適当におかしくした般若心経
   const edittedShinkyo = correctText.map((x, index) => {
     return !(x.char === "・" || x.char === "、" || x.char === "。")
       ? !(Math.random() > 0.90)
@@ -21,8 +24,16 @@ const App = () => {
       : x
   })
 
+  // 精度計算
+  // edittedパラメータがfalseのもののみ抽出した配列の長さ（=正しい般若心経の文字数）
+  const isFalseArrayLength = textArray => textArray.filter(x => !x.editted).length;
+  // 上記関数から全体のどのくらい正しい般若心経になっているかのチェック
+  const culcTextAccuracy = (edittedText, correctText) => (isFalseArrayLength(edittedText) * 100) / isFalseArrayLength(correctText)
+
   // クリック管理
+  // 現在の般若心経管理
   const [edittedText, setEdittedText] = useState(edittedShinkyo)
+  // クリック時にクリックした箇所のedittedパラメータを切り替えてstateに入れる関数
   const handleEdittedText = index => {
     const nowArray = [...edittedText]
     const newObj = { ...nowArray[index], editted: nowArray[index].editted ? false : true, clicked: nowArray[index].clicked === "" ? "clicked" : "" };
@@ -35,9 +46,7 @@ const App = () => {
       <main className="App-main">
         <h1>Master of Heart Sutra</h1>
         <p>間違っている文字をクリック！</p>
-        {/* <p>{correctText.filter(x => !x.editted).length}</p> */}
-        {/* <p>{edittedText.filter(x => !x.editted).length}</p> */}
-        <p>現在の精度：{((edittedText.filter(x => !x.editted).length) * 100 / (correctText.filter(x => !x.editted).length)).toFixed(2)} %</p>
+        <p>現在の精度：{culcTextAccuracy(edittedText, correctText).toFixed(2)} %</p>
         <h2>
           <code>
             {shinkyo_title.map(x => x.char).join('')}
@@ -45,7 +54,7 @@ const App = () => {
         </h2>
         <div>
           <p>
-            {edittedText.map((x, index) => <span key={index} className={x.clicked} onClick={() => handleEdittedText(index)}>{x.char}</span>)}
+            {edittedText.map((x, index) => <code key={index} className={x.clicked} onClick={() => handleEdittedText(index)}>{x.char}</code>)}
           </p>
         </div>
         {/* <p>
